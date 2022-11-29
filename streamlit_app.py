fc01ea4e80e96cc42dfe9d3da8840f18fa756147
Kris V <119368052+kvasired1977@users.noreply.github.com>
2022-11-29 12:34:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas
 streamlit.title('My parents healthy diner')
 streamlit.header('😀Header')
 streamlit.text('test for text')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
